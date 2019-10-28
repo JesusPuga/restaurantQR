546bce9b33e3e1417c7b34529e79764412f86095
JesusPuga <jesuspuga01@gmail.com>
2019-10-27 22:30:34 -0600

    Added template to buy meals

---
+++ resources/views/index.blade.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Antojitos Express</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Free HTML5 Website Template by uicookies.com" />
+    <meta name="keywords" content="free bootstrap 4, free bootstrap 4 template, free website templates, free html5, free template, free website template, html5, css3, mobile first, responsive" />
+    <meta name="author" content="uicookies.com" />
+    
+    <link href="https://fonts.googleapis.com/css?family=Work+Sans" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{ url('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ url('css/open-iconic-bootstrap.min.css') }}">
+    
+    <link rel="stylesheet" href="{{ url('css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ url('css/owl.theme.default.min.css') }}">
+
+    <link rel="stylesheet" href="{{ url('css/icomoon.css') }}">
+    <link rel="stylesheet" href="{{ url('css/animate.css') }}">
+    <link rel="stylesheet" href="{{ url('css/style.css') }}">
+
+  </head>
+  <body>
+    
+    <aside class="probootstrap-aside js-probootstrap-aside">
+      <a href="#" class="probootstrap-close-menu js-probootstrap-close-menu d-md-none"><span class="oi oi-arrow-left"></span> Close</a>
+      <div class="probootstrap-site-logo probootstrap-animate" data-animate-effect="fadeInLeft">
+        
+        <a href="index.html" class="mb-2 d-block probootstrap-logo">Antojitos Express</a>
+      </div>
+      <div class="probootstrap-overflow">
+        <nav class="probootstrap-nav">
+          <ul>
+            <li class="probootstrap-animate active" data-animate-effect="fadeInLeft"><a href="index.html">Mesa X</a></li>
+          </ul>
+        </nav>
+        <footer class="probootstrap-aside-footer probootstrap-animate" data-animate-effect="fadeInLeft">
+          <ul class="list-unstyled d-flex probootstrap-aside-social">
+            <li><a href="#" class="p-2"><span class="icon-twitter"></span></a></li>
+            <li><a href="#" class="p-2"><span class="icon-instagram"></span></a></li>
+            <li><a href="#" class="p-2"><span class="icon-dribbble"></span></a></li>
+          </ul>
+          <p>&copy; 2017 <a href="https://uicookies.com/" target="_blank">uiCookies:Aside</a>. <br> All Rights Reserved.</p>
+        </footer>
+      </div>
+    </aside>
+
+
+    <main role="main" class="probootstrap-main js-probootstrap-main">
+      <div class="probootstrap-bar">
+        <a href="#" class="probootstrap-toggle js-probootstrap-toggle"><span class="oi oi-menu"></span></a>
+        <div class="probootstrap-main-site-logo"><a href="index.html">Aside</a></a></div>
+      </div>
+      <div class="row p-1">
+        <div class="col-ms-12">
+            <button class="btn btn-primary" onclick="BuyMeals()">Realizar compra</button>
+        </div>
+      </div>
+      <br>
+      <div class="card-columns">
+        <div class="card" onclick="AddedMessage()">
+          <a>
+          <img class="card-img-top probootstrap-animate" src="{{ url('images/img_1.jpg') }}" alt="Card image cap">
+          </a>
+        </div>
+        <div class="card" onclick="AddedMessage()">
+          <a>
+            <img class="card-img-top probootstrap-animate" src="{{ url('images/img_2.jpg') }}" alt="Card image cap">
+          </a>
+        </div>
+        <div class="card" onclick="AddedMessage()">
+          <a>
+            <img class="card-img-top probootstrap-animate" src="{{ url('images/img_3.jpg') }}" alt="Card image cap">
+          </a>
+        </div>
+        <div class="card" onclick="AddedMessage()">
+          <a >
+            <img class="card-img-top probootstrap-animate" src="{{ url('images/img_4.jpg') }}" alt="Card image cap">
+          </a>
+        </div>
+        <div class="card" onclick="AddedMessage()">
+          <a >
+            <img class="card-img-top probootstrap-animate" src="{{ url('images/img_5.jpg') }}" alt="Card image cap">
+          </a>
+        </div>
+        <div class="card" onclick="AddedMessage()">
+          <a >
+            <img class="card-img-top probootstrap-animate" src="{{ url('images/img_6.jpg') }}" alt="Card image cap">
+          </a>
+        </div>
+        <div class="card" onclick="AddedMessage()">
+          <a >
+            <img class="card-img-top probootstrap-animate" src="{{ url('images/img_7.jpg') }}" alt="Card image cap">
+          </a>
+        </div>
+        <div class="card" onclick="AddedMessage()">
+          <a >
+            <img class="card-img-top probootstrap-animate" src="{{ url('images/img_8.jpg') }}" alt="Card image cap">
+          </a>
+        </div>
+        <div class="card" onclick="AddedMessage()">
+          <a >
+            <img class="card-img-top probootstrap-animate" src="{{ url('images/img_9.jpg') }}" alt="Card image cap">
+          </a>
+        </div>
+        <div class="card" onclick="AddedMessage()">
+          <a >
+            <img class="card-img-top probootstrap-animate" src="{{ url('images/img_10.jpg') }}" alt="Card image cap">
+          </a>
+        </div>
+        <div class="card" onclick="AddedMessage()">
+          <a >
+            <img class="card-img-top probootstrap-animate" src="{{ url('images/img_11.jpg') }}" alt="Card image cap">
+          </a>
+        </div>
+        <div class="card" onclick="AddedMessage()">
+          <a >
+            <img class="card-img-top probootstrap-animate" src="{{ url('images/img_12.jpg') }}" alt="Card image cap">
+          </a>
+        </div>
+        <div class="card" onclick="AddedMessage()">
+          <a >
+            <img class="card-img-top probootstrap-animate" src="{{ url('images/img_13.jpg') }}" alt="Card image cap">
+          </a>
+        </div>
+        <div class="card" onclick="AddedMessage()">
+          <a >
+            <img class="card-img-top probootstrap-animate" src="{{ url('images/img_14.jpg') }}" alt="Card image cap">
+          </a>
+        </div>
+        <div class="card" onclick="AddedMessage()">
+          <a >
+            <img class="card-img-top probootstrap-animate" src="{{ url('images/img_15.jpg') }}" alt="Card image cap">
+          </a>
+        </div>
+        <div class="card" onclick="AddedMessage()">
+          <a >
+            <img class="card-img-top probootstrap-animate" src="{{ url('images/img_16.jpg') }}" alt="Card image cap">
+          </a>
+        </div>
+        <div class="card" onclick="AddedMessage()">
+          <a >
+            <img class="card-img-top probootstrap-animate" src="{{ url('images/img_17.jpg') }}" alt="Card image cap">
+          </a>
+        </div>
+        <div class="card" onclick="AddedMessage()">
+          <a >
+            <img class="card-img-top probootstrap-animate" src="{{ url('images/img_18.jpg') }}" alt="Card image cap">
+          </a>
+        </div>
+        <div class="card" onclick="AddedMessage()">
+          <a >
+            <img class="card-img-top probootstrap-animate" src="{{ url('images/img_19.jpg') }}" alt="Card image cap">
+          </a>
+        </div>
+        <div class="card" onclick="AddedMessage()">
+          <a >
+            <img class="card-img-top probootstrap-animate" src="{{ url('images/img_20.jpg') }}" alt="Card image cap">
+          </a>
+        </div>
+        <div class="card" onclick="AddedMessage()">
+          <a >
+            <img class="card-img-top probootstrap-animate" src="{{ url('images/img_21.jpg') }}" alt="Card image cap">
+          </a>
+        </div>
+      </div>  
+
+      <div class="container-fluid d-md-none">
+        <div class="row">
+          <div class="col-md-12">
+            <ul class="list-unstyled d-flex probootstrap-aside-social">
+              <li><a href="#" class="p-2"><span class="icon-twitter"></span></a></li>
+              <li><a href="#" class="p-2"><span class="icon-instagram"></span></a></li>
+              <li><a href="#" class="p-2"><span class="icon-dribbble"></span></a></li>
+            </ul>
+            <p>&copy; 2017 <a href="https://uicookies.com/" target="_blank">uiCookies:Aside</a>. <br> All Rights Reserved. Designed by <a href="https://uicookies.com/" target="_blank">uicookies.com</a></p>
+          </div>
+        </div>
+      </div>
+
+    </main>
+
+    
+
+    <script src="{{ url('js/jquery-3.2.1.slim.min.js') }}"></script>
+    <script src="{{ url('js/popper.min.js') }}"></script>
+    <script src="{{ url('js/bootstrap.min.js') }}"></script>
+    <script src="{{ url('js/owl.carousel.min.js') }}"></script>
+    <script src="{{ url('js/jquery.waypoints.min.js') }}"></script>
+    <script src="{{ url('js/imagesloaded.pkgd.min.js') }}"></script>
+
+    <script src="{{ url('js/main.js') }}"></script>
+    
+  </body>
+</html>
